16afcc71ab226155211e4aa3d726a3e419f99a47
Michael Specht <micha.specht@gmail.com>
2021-02-03 14:35:48 +0100

    yay

--- src/scripts/clear-database.rb
+++ src/scripts/clear-database.rb
@@ -1,3 +1,3 @@
 #!/usr/bin/env ruby
 
-system("cd ../.. && ./config.rb exec ruby ruby clear-database.rb && cd src/scripts")
+system("cd ../.. && ./config.rb exec ruby ruby clear-database.rb #{ARGV[1, ARGV.size - 1].join(' ')} && cd src/scripts")

aa80484b3889cc93f644db30f917d699b80f5b01
Michael Specht <micha.specht@gmail.com>
2021-02-05 00:22:57 +0100

    yay

--- src/scripts/create-nc-users.rb
+++ src/scripts/create-nc-users.rb
@@ -1,3 +1,3 @@
 #!/usr/bin/env ruby
 
-system("cd ../.. && ./config.rb exec ruby ruby create-nc-users.rb && cd src/scripts")
+system("cd ../.. && ./config.rb exec ruby ruby create-nc-users.rb #{ARGV.join(' ')} && cd src/scripts")

c013a56287e8ddf2df4f61c223dcf05490cce784
Michael Specht <micha.specht@gmail.com>
2021-04-21 01:02:15 +0200

    yay

--- src/ruby/timetable.rb
+++ src/ruby/timetable.rb
@@ -341,7 +341,8 @@ class Timetable
                     added_klassen = event[:klassen].last - event[:klassen].first
                     added_klassen.each do |klasse|
                         stunde = ventry[:stunde]
-                            
+
+                        STDERR.puts "[#{hfk_ds}] [#{klasse}] [#{stunde - 1}]"
                         start_time = HOURS_FOR_KLASSE[hfk_ds][klasse][stunde - 1][0]
                         end_time = HOURS_FOR_KLASSE[hfk_ds][klasse][stunde - 1][1]
                         

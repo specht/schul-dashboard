56e9a061ee255a2461354565433af9d34c7f6e41
Michael Specht <micha.specht@gmail.com>
2021-11-22 08:24:19 +0100

    yay

---
+++ src/ruby/dump-anmeldungen.rb
@@ -0,0 +1,23 @@
+#!/usr/bin/env ruby
+SKIP_COLLECT_DATA = true
+require './main.rb'
+
+class DumpDatabase
+    include QtsNeo4j
+    
+    def run
+        transaction do
+            rows = neo4j_query(<<~END_OF_QUERY, :mode => ARGV.first).map { |x| x['n'].props }
+                MATCH (n:PublicEventPerson {mode: $mode})-[:SIGNED_UP_FOR]->(e:PublicEvent {name: "Info-Abend für Viertklässler-Eltern"})
+                RETURN n
+                ORDER BY n.timestamp;
+            END_OF_QUERY
+            rows.each do |row|
+                puts "#{row[:name]} <#{row[:email]}>"
+            end
+        end
+    end
+end
+
+dump = DumpDatabase.new
+dump.run

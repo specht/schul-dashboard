92c5d3514f65f47d18b862e962ca226273ac8f5e
Michael Specht <micha.specht@gmail.com>
2022-01-25 15:55:35 +0100

    yay

--- src/ruby/include/salzh.rb
+++ src/ruby/include/salzh.rb
@@ -1,11 +1,12 @@
 class Main < Sinatra::Base
     post '/api/update_salzh_for_sus' do
         require_user_who_can_manage_salzh!
-        data = parse_request_data(:required_keys => [:email, :end_date])
-        neo4j_query_expect_one(<<~END_OF_QUERY, :email => data[:email], :end_date => data[:end_date])
+        data = parse_request_data(:required_keys => [:email, :end_date, :mode])
+        neo4j_query_expect_one(<<~END_OF_QUERY, :email => data[:email], :end_date => data[:end_date], :mode => data[:mode])
             MATCH (u:User {email: {email}})
             MERGE (s:Salzh)-[:BELONGS_TO]->(u)
             SET s.end_date = {end_date}
+            SET s.mode = {mode}
             RETURN s;
         END_OF_QUERY
         respond(:ok => true)
@@ -39,14 +40,24 @@ class Main < Sinatra::Base
             {
                 :email => email,
                 :name => @@user_info[email][:display_name],
+                :first_name => @@user_info[email][:display_first_name],
+                :last_name => @@user_info[email][:display_last_name],
                 :klasse => @@user_info[email][:klasse],
+                :klasse_tr => tr_klasse(@@user_info[email][:klasse]),
+                :mode => x['s'].props[:mode] || 'salzh',
                 :end_date => x['s'].props[:end_date]
             }
         end
         entries.sort! do |a, b|
-            (a[:klasse] == b[:klasse]) ?
-            (a[:name] <=> b[:name]) :
-            (KLASSEN_ORDER.index(a[:klasse]) <=> KLASSEN_ORDER.index(b[:klasse]))
+            if a[:klasse] == b[:klasse]
+                if a[:last_name] == b[:last_name]
+                    a[:first_name] <=> b[:first_name]
+                else
+                    a[:last_name] <=> b[:last_name]
+                end
+            else
+                (KLASSEN_ORDER.index(a[:klasse]) <=> KLASSEN_ORDER.index(b[:klasse]))
+            end
         end
         entries
     end
@@ -97,20 +108,78 @@ class Main < Sinatra::Base
             return '' if entries.empty?
             StringIO.open do |io|
                 io.puts "<div class='hint'>"
-                io.puts "<p><strong><div style='display: inline-block; padding: 4px; margin: -4px; border-radius: 4px' class='bg-warning'>SuS im saLzH</div></strong></p>"
+                # io.puts "<p><strong><div style='display: inline-block; padding: 4px; margin: -4px; border-radius: 4px' class='bg-warning'>SuS im saLzH</div></strong></p>"
+                contact_person_count = 0
+                salzh_count = 0
                 all_klassen = {}
+                entry_for_email = {}
+                email_for_klasse_and_mode = {}
                 entries.each do |x| 
+                    entry_for_email[x[:email]] = x
+                    if x[:mode] == 'contact_person'
+                        contact_person_count += 1
+                    else
+                        salzh_count += 1
+                    end
                     all_klassen[x[:klasse]] ||= []
                     all_klassen[x[:klasse]] << x[:email]
+                    email_for_klasse_and_mode[x[:klasse]] ||= {}
+                    email_for_klasse_and_mode[x[:klasse]][x[:mode]] ||= []
+                    email_for_klasse_and_mode[x[:klasse]][x[:mode]] << x[:email]
                 end
-                io.puts "<p>Momentan befinde#{entries.size == 1 ? 't' : 'n'} sich <strong>#{entries.size} SuS</strong> in <strong>#{all_klassen.size} Klasse#{all_klassen.size == 1 ? '' : 'n'}</strong> im pandemiebedingten saLzH. Bitte ermöglichen Sie diesen SuS eine Teilnahme am Unterricht im saLzH.</p>"
+
+                io.puts "<p>"
+                if contact_person_count > 0
+                    io.puts "Sie haben momentan <span class='bg-warning' style='padding: 0.2em 0.5em; font-weight: bold; border-radius: 0.25em;'>#{contact_person_count}&nbsp;SuS</span>, die Kontaktpersonen sind."
+                end
+                if salzh_count > 0
+                    io.puts "Sie haben momentan <span class='bg-danger' style='padding: 0.2em 0.5em; font-weight: bold; border-radius: 0.25em; color: #fff;'>#{salzh_count}&nbsp;SuS</span>, die im saLzH sind."
+                end
+                io.puts "</p>"
                 io.puts "<div style='margin: 0 -10px 0 -10px;'><table class='table table-narrow narrow'>"
-                # io.puts "<tr><th>Klasse</th><th>SuS</th></tr>"
+                io.puts "<tr><th>Klasse</th><th>Status</th></tr>"
                 KLASSEN_ORDER.each do |klasse|
                     next unless all_klassen.include?(klasse)
-                    io.puts "<tr class='klasse-click-row' data-klasse='#{klasse}'><td><strong>Klasse #{tr_klasse(klasse)}</strong></td><td>#{all_klassen[klasse].size} SuS</td></tr>"
+                    io.puts "<tbody>"
+                    io.puts "<tr class='klasse-click-row' data-klasse='#{klasse}'><td><strong>Klasse #{tr_klasse(klasse)}</strong></td>"
+                    io.puts "<td>"
+                    ['contact_person', 'salzh'].each do |mode|
+                        io.puts "<span class='salzh-badge salzh-badge-big bg-#{mode == 'contact_person' ? 'warning': 'danger'}'><span>#{(email_for_klasse_and_mode[klasse][mode] || []).size}</span></span>"
+                    end
+                    io.puts "</td></tr>"
+                    io.puts "</tbody>"
+                    io.puts "<tbody style='display: none;'>"
+                    all_klassen[klasse].each do |email|
+                        badge =  
+                        badge = "<span class='salzh-badge #{entry_for_email[email][:mode] == 'contact_person' ? 'bg-warning' : 'bg-danger'}'></span>"
+                        io.puts "<tr><td colspan='2'>#{badge}#{@@user_info[email][:display_name]}</td></tr>"
+                    end
+                    io.puts "</tbody>"
                 end
                 io.puts "</table></div>"
+                io.puts "<hr />"
+                io.puts "<p>"
+                io.puts "<strong>Was bedeutet das?</strong>"
+                io.puts "</p>"
+                if contact_person_count > 0
+                    io.puts "<hr />"
+                    io.puts "<p>"
+                    io.puts "<strong><span class='bg-danger' style='padding: 0.2em 0.5em; font-weight: bold; border-radius: 0.25em; color: #fff;'>saLzH:</span></strong> Es handelt sich um SuS, die entweder positiv getestet wurden oder als Kontaktperson nach Rückmeldung der Eltern bestätigt freiwillig im saLzH sind. Bitte ermöglichen Sie diesen SuS eine Teilnahme am Unterricht."
+                    io.puts "</p>"
+                end
+                if contact_person_count > 0
+                    io.puts "<hr />"
+                    io.puts "<p>"
+                    io.puts "<strong><span class='bg-warning' style='padding: 0.2em 0.5em; font-weight: bold; border-radius: 0.25em;'>Kontaktpersonen:</span></strong> Diese SuS wurden als Kontaktperson identifiziert, besuchen aber trotzdem weiterhin die Schule. Für sie gilt:"
+                    io.puts "</p>"
+                    io.puts "<ul style='padding-left: 1.5em;'>"
+                    io.puts "<li>tägliche Testung vor Beginn der Schultages (ein Test vom Vortag, z. B. aus einem Schnelltestzentrum, kann bei diesen SuS nicht akzeptiert werden)</li>"
+                    io.puts "<li>durch die Klassenleitung wird am Freitag für jeden Tag des Wochenendes, der in diesen Status fällt, ein Schnelltest mitgegeben</li>"
+                    io.puts "<li>zeigen diese Kinder Symptome (Husten, Fieber, Kopfschmerzen, …), dürfen sie das Schulhaus nicht mehr betreten</li>"
+                    io.puts "<li>am gemeinsamen Essen in der Mensa darf nicht mehr teilgenommen werden</li>"
+                    io.puts "<li>während des Sportunterrichts (Umkleide, Sport in der Halle) muss durchgehend eine Maske getragen werden – ist dies aufgrund der körperlichen Betätigung nicht möglich, nehmen diese Kinder nicht am Sportunterricht teil</li>"
+                    io.puts "</ul>"
+                end
                 io.puts "</div>"
                 io.string
             end

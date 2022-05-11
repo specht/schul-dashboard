b1ba15d7f5292d0901060d5ecabaf4ed0671d692
Michael Specht <micha.specht@gmail.com>
2022-05-11 16:46:51 +0200

    yay

--- src/ruby/include/tablet_set.rb
+++ src/ruby/include/tablet_set.rb
@@ -275,9 +275,11 @@ class Main < Sinatra::Base
             hints = []
             if booked_tablet_sets_timespan[x]
                 booked_tablet_sets_timespan[x].to_a.each do |entry|
-                    if entry[:lesson_key] != @session_user[:lesson_key]
+                    if entry[:lesson_key]
                         pretty_fach = fach_for_lesson_key(entry[:lesson_key])
-                        hints << "<span class='text-danger'><i class='fa fa-warning'></i></span>&nbsp;&nbsp;Dieser Tabletsatz wurde bereits von #{entry[:display_name]} gebucht#{entry[:lesson_key] ? ': ' + pretty_fach : ''}"
+                        hints << "<span class='text-danger'><i class='fa fa-warning'></i></span>&nbsp;&nbsp;Dieser Tabletsatz wurde bereits von <strong>#{entry[:display_name]}</strong> gebucht#{entry[:lesson_key] ? ': ' + pretty_fach : ''}."
+                    else
+                        hints << "<span class='text-danger'><i class='fa fa-warning'></i></span>&nbsp;&nbsp;Dieser Tabletsatz wurde bereits von <strong>#{entry[:display_name]}</strong> gebucht."
                     end
                 end
             elsif booked_tablet_sets_day[x]

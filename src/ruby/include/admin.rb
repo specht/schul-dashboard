7d6813075c51cb3d2ab08a3f1f3f7d42851be4d6
Michael Specht <micha.specht@gmail.com>
2022-09-29 19:43:10 +0200

    yay

--- src/ruby/include/admin.rb
+++ src/ruby/include/admin.rb
@@ -113,6 +113,8 @@ class Main < Sinatra::Base
             io.puts "<a class='btn btn-secondary' href='#monitor'>Monitor</a>"
             io.puts "<a class='btn btn-secondary' href='#bibliothek'>Bibliothek</a>"
             io.puts "<a class='btn btn-secondary' href='/lesson_keys'>Lesson Keys</a>"
+            io.puts "<a class='btn btn-secondary' href='/api/all_sus_logo_didact'>LDC: Alle SuS</a>"
+            io.puts "<a class='btn btn-secondary' href='/api/all_lul_logo_didact'>LDC: Alle Lehrkräfte</a>"
             io.puts "<hr />"
             io.puts "<h3 id='teachers'>Lehrerinnen und Lehrer</h3>"
             io.puts "<div style='max-width: 100%; overflow-x: auto;'>"
@@ -363,7 +365,7 @@ class Main < Sinatra::Base
         respond_raw_with_mimetype(print_all_users, 'text/plain')
     end
 
-    def print_all_users_logo_didact
+    def print_all_sus_logo_didact
         require_admin!
         StringIO.open do |io|
             @@klassen_order.each do |klasse|
@@ -378,9 +380,9 @@ class Main < Sinatra::Base
         end
     end
 
-    get '/api/all_users_logo_didact' do
+    get '/api/all_sus_logo_didact' do
         require_admin!
-        respond_raw_with_mimetype(print_all_users_logo_didact, 'text/plain')
+        respond_raw_with_mimetype(print_all_sus_logo_didact, 'text/plain')
     end
 
     def print_email_accounts()

73688f719c3cc1abecb211fbea23990bf970277a
Michael Specht <micha.specht@gmail.com>
2023-03-10 09:25:53 +0100

    yay

--- src/ruby/include/color.rb
+++ src/ruby/include/color.rb
@@ -128,7 +128,7 @@ class Main < Sinatra::Base
         contrast_color = rgb_to_hex(mix(hex_to_rgb(primary_color), color_scheme[0] == 'l' ? [0, 0, 0] : [255, 255, 255], 0.7))
         color_palette = {
             :is_light => light,
-            :primary => primary_color, 
+            :primary => light ? primary_color : darken(primary_color, 0.5),
             :primary_color_darker => primary_color_darker,
             :disabled => disabled_color, 
             :darker => darker_color, 

caf71198ab38a44ff938921a6e1c4aeca9bd84c9
Peter-J. G <peter.germelmann@gmail.com>
2023-04-23 21:53:52 +0200

    Admin-Bereich für user_who_can_manage_tablets

--- src/ruby/include/techpost.rb
+++ src/ruby/include/techpost.rb
@@ -9,13 +9,13 @@ class Main < Sinatra::Base
         end
         code = possible_codes.sample
 
-        probelm_data = {
+        problem_data = {
             :token => RandomTag.generate(24),
             :problem => data[:problem],
             :date => data[:date],
             :device => data[:device],
         }
-        File.open(sprintf('/internal/tech_problem/%04d.json', code), 'w') { |f| f.write(probelm_data.to_json) }
+        File.open(sprintf('/internal/tech_problem/%04d.json', code), 'w') { |f| f.write(problem_data.to_json) }
         neo4j_query(<<~END_OF_QUERY, :code => code, :email => @session_user[:email])
             MATCH (u:User {email: $email})
             CREATE (v:TechProblem {code: $code})-[:BELONGS_TO]->(u)
@@ -31,13 +31,13 @@ class Main < Sinatra::Base
         END_OF_QUERY
         results = codes.map do |code|
             begin
-                probelm_data = {}
+                problem_data = {}
                 STDERR.puts "/api/get_tech_problems: #{code}"
                 File.open(sprintf("/internal/tech_problem/%04d.json", code)) do |f|
-                    probelm_data = JSON.parse(f.read)
+                    problem_data = JSON.parse(f.read)
                 end
-                probelm_data[:code] = code
-                probelm_data
+                problem_data[:code] = code
+                problem_data
             rescue 
                 STDERR.puts "Unable to read #{sprintf('/internal/tech_problem/%04d.json', code)}"
                 nil
@@ -49,6 +49,32 @@ class Main < Sinatra::Base
         respond(:tech_problems => results)
     end
     
+    post '/api/get_tech_problems_admin' do
+        require_user_who_can_manage_tablets!
+        codes = neo4j_query(<<~END_OF_QUERY, :email => @session_user[:email]).map { |x| x['v.code'] }
+            MATCH (v:TechProblem)
+            RETURN v.code;
+        END_OF_QUERY
+        results = codes.map do |code|
+            begin
+                problem_data = {}
+                STDERR.puts "/api/get_tech_problems_admin: #{code}"
+                File.open(sprintf("/internal/tech_problem/%04d.json", code)) do |f|
+                    problem_data = JSON.parse(f.read)
+                end
+                problem_data[:code] = code
+                problem_data
+            rescue 
+                STDERR.puts "Unable to read #{sprintf('/internal/tech_problem/%04d.json', code)}"
+                nil
+            end
+        end.reject { |x| x.nil? }
+        results = results.sort do |a, b|
+            a['date'] <=> b['date']
+        end
+        respond(:tech_problems => results)
+    end
+
     post '/api/delete_tech_problem' do
         require_user_who_can_manage_tablets!
         data = parse_request_data(:required_keys => [:code],
@@ -64,4 +90,19 @@ class Main < Sinatra::Base
         FileUtils::rm_f(sprintf('/internal/tech_problem/%04d.pdf', code));
         respond(:ok => true)
     end
+
+    post '/api/delete_tech_problem_admin' do
+        require_user_who_can_manage_tablets!
+        data = parse_request_data(:required_keys => [:code],
+                                  :types => {:code => Integer})
+        code = data[:code]
+        neo4j_query(<<~END_OF_QUERY, :code => code)
+            MATCH (v:TechProblem {code: $code})
+            DETACH DELETE v
+        END_OF_QUERY
+        STDERR.puts "Deleting #{code}..."
+        FileUtils::rm_f(sprintf('/internal/tech_problem/%04d.json', code));
+        respond(:ok => true)
+    end
+
 end

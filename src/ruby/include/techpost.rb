2b956b42473bfd6998e3e54db4bae210fe84c799
Peter-J. G <107960207+gpeter2706@users.noreply.github.com>
2023-04-25 21:12:28 +0200

    Update techpost.rb

--- src/ruby/include/techpost.rb
+++ src/ruby/include/techpost.rb
@@ -1,109 +1,87 @@
 class Main < Sinatra::Base
     post '/api/report_tech_problem' do
-        require_techpost_user!
+        require_user_who_can_report_tech_problems!
         data = parse_request_data(:required_keys => [:problem, :date, :device],)
-        possible_codes = (0..9999).to_a
-        Dir['/internal/tech_problem/*.json'].each do |path|
-            code = File.basename(path).gsub('.json', '').to_i
-            possible_codes.delete(code)
-        end
-        code = possible_codes.sample
 
-        problem_data = {
-            :user => @session_user[:display_name],
-            :class => @session_user[:klasse],
-            :token => RandomTag.generate(24),
-            :problem => data[:problem],
-            :date => data[:date],
-            :device => data[:device],
-        }
-        File.open(sprintf('/internal/tech_problem/%04d.json', code), 'w') { |f| f.write(problem_data.to_json) }
-        neo4j_query(<<~END_OF_QUERY, :code => code, :email => @session_user[:email])
+        token = RandomTag.generate(24)
+        neo4j_query_expect_one(<<~END_OF_QUERY, :token => token, :email => @session_user[:email], :device => data[:device], :date => data[:date], :problem => data[:problem])
             MATCH (u:User {email: $email})
-            CREATE (v:TechProblem {code: $code})-[:BELONGS_TO]->(u)
+            CREATE (v:TechProblem {token: $token})-[:BELONGS_TO]->(u)
+            SET v.device = $device 
+            SET v.date = $date
+            SET v.problem = $problem
+            RETURN v.token;
         END_OF_QUERY
+
+        deliver_mail do
+            to CAN_MANAGE_TABLETS_USERS
+            bcc SMTP_FROM
+            from SMTP_FROM
+
+            subject "Neues Technikproblem"
+
+            StringIO.open do |io|
+                io.puts "<p>Hallo TechnikTeam, "
+                io.puts "<p>es liegt ein neues Technikproblem vor.</p>"
+                io.puts "<p>Das Problem betrifft #{data[:device]} und lautet: „#{data[:problem]}“</p>"
+                io.puts "<p>Viele Grüße</p>"
+                io.string
+            
+            end
+        end
         respond(:ok => true)
     end
 
     post '/api/get_tech_problems' do
-        require_techpost_user!
-        codes = neo4j_query(<<~END_OF_QUERY, :email => @session_user[:email]).map { |x| x['v.code'] }
+        require_user_who_can_report_tech_problems!
+        problems = neo4j_query(<<~END_OF_QUERY, :email => @session_user[:email]).map { |x| {:problem => x['v'], :email => x['u.email']} }
             MATCH (v:TechProblem)-[:BELONGS_TO]->(:User {email: $email})
-            RETURN v.code;
+            RETURN v;
         END_OF_QUERY
-        results = codes.map do |code|
-            begin
-                problem_data = {}
-                STDERR.puts "/api/get_tech_problems: #{code}"
-                File.open(sprintf("/internal/tech_problem/%04d.json", code)) do |f|
-                    problem_data = JSON.parse(f.read)
-                end
-                problem_data[:code] = code
-                problem_data
-            rescue 
-                STDERR.puts "Unable to read #{sprintf('/internal/tech_problem/%04d.json', code)}"
-                nil
-            end
-        end.reject { |x| x.nil? }
-        results = results.sort do |a, b|
-            a['date'] <=> b['date']
-        end
-        respond(:tech_problems => results)
+
+        debug problems.to_yaml
+        respond(:tech_problems => problems)
     end
     
     post '/api/get_tech_problems_admin' do
         require_user_who_can_manage_tablets!
-        codes = neo4j_query(<<~END_OF_QUERY, :email => @session_user[:email]).map { |x| x['v.code'] }
-            MATCH (v:TechProblem)
-            RETURN v.code;
+        problems = neo4j_query(<<~END_OF_QUERY, :email => @session_user[:email]).map { |x| {:problem => x['v'], :email => x['u.email']} }
+            MATCH (v:TechProblem)-[:BELONGS_TO]->(u:User)
+            RETURN v, u.email;
         END_OF_QUERY
-        results = codes.map do |code|
-            begin
-                problem_data = {}
-                STDERR.puts "/api/get_tech_problems_admin: #{code}"
-                File.open(sprintf("/internal/tech_problem/%04d.json", code)) do |f|
-                    problem_data = JSON.parse(f.read)
-                end
-                problem_data[:code] = code
-                problem_data
-            rescue 
-                STDERR.puts "Unable to read #{sprintf('/internal/tech_problem/%04d.json', code)}"
-                nil
-            end
-        end.reject { |x| x.nil? }
-        results = results.sort do |a, b|
-            a['date'] <=> b['date']
+        problems.map! do |x|
+            x[:display_name] = @@user_info[x[:email]][:display_name]
+            x[:klasse] = @@user_info[x[:email]][:klasse]
+            x
         end
-        respond(:tech_problems => results)
+
+        debug problems.to_yaml
+        respond(:tech_problems => problems)
     end
 
     post '/api/delete_tech_problem' do
-        require_techpost_user!
-        data = parse_request_data(:required_keys => [:code],
-                                  :types => {:code => Integer})
-        code = data[:code]
-        neo4j_query_expect_one(<<~END_OF_QUERY, :code => code, :email => @session_user[:email])
-            MATCH (v:TechProblem {code: $code})-[:BELONGS_TO]->(u:User {email: $email})
+        require_user_who_can_report_tech_problems!
+        data = parse_request_data(:required_keys => [:token])
+        token = data[:token]
+        debug token
+        neo4j_query_expect_one(<<~END_OF_QUERY, :token => token)
+            MATCH (v:TechProblem {token: $token})
             DETACH DELETE v
-            RETURN u.email;
+            RETURN v;
         END_OF_QUERY
-        STDERR.puts "Deleting #{code}..."
-        FileUtils::rm_f(sprintf('/internal/tech_problem/%04d.json', code));
-        FileUtils::rm_f(sprintf('/internal/tech_problem/%04d.pdf', code));
         respond(:ok => true)
     end
 
     post '/api/delete_tech_problem_admin' do
         require_user_who_can_manage_tablets!
-        data = parse_request_data(:required_keys => [:code],
-                                  :types => {:code => Integer})
-        code = data[:code]
-        neo4j_query(<<~END_OF_QUERY, :code => code)
-            MATCH (v:TechProblem {code: $code})
+        data = parse_request_data(:required_keys => [:token])
+        token = data[:token]
+        debug token
+        problems = neo4j_query_expect_one(<<~END_OF_QUERY, :token => token)
+            MATCH (v:TechProblem {token: $token})
             DETACH DELETE v
+            RETURN v;
         END_OF_QUERY
-        STDERR.puts "Deleting #{code}..."
-        FileUtils::rm_f(sprintf('/internal/tech_problem/%04d.json', code));
         respond(:ok => true)
     end
 

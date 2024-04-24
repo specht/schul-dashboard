728c82acc0df48e0994f6cca73eb081d53ced0c2
Michael Specht <micha.specht@gmail.com>
2024-04-24 12:25:40 +0200

    yay

--- src/ruby/send-bibliothek-zahlungsaufforderung.rb
+++ src/ruby/send-bibliothek-zahlungsaufforderung.rb
@@ -18,6 +18,9 @@ BEITRAG_SESB_1 = 50
 BEITRAG_SESB_2 = 40
 BEITRAG_SESB_3 = 40
 
+LETTERS = 'BCDFHJLMNPQRSTVWYZ'
+DIGITS = '23456789'
+
 class Script
     def run
 
@@ -56,11 +59,28 @@ class Script
             ((@@klassen_order.index(a[:klasse]) || -1) <=> (@@klassen_order.index(b[:klasse]) || -1))
         end.each do |record|
             email = record[:email]
-            next if (@@user_info[email][:siblings_next_year] || []).empty?
-            STDERR.puts record.to_yaml
+            # next if (@@user_info[email][:siblings_next_year] || []).empty?
+            next unless email == 'carolina.hoffmann@mail.gymnasiumsteglitz.de'
+            subject_i = Digest::SHA1.hexdigest("BIB/#{NEXT_SCHULJAHR}/#{email}").to_i(16)
+            subject = ''
+            2.times do
+                subject += LETTERS[subject_i % LETTERS.length]
+                subject_i /= LETTERS.length
+            end
+            subject += '-'
+            2.times do
+                subject += LETTERS[subject_i % LETTERS.length]
+                subject_i /= LETTERS.length
+            end
+            subject += '-'
+            3.times do
+                subject += DIGITS[subject_i % DIGITS.length]
+                subject_i /= DIGITS.length
+            end
             sesb = sesb_sus.include?(email)
             klassenstufe = record[:klasse].to_i
             klassenstufe_next = klassenstufe + 1
+            next if klassenstufe_next < 7
             STDERR.puts "next: #{klassenstufe_next}"
             brief_id = "#{ZEUGNIS_SCHULJAHR}/Beitragsaufforderung/#{email}"
             brief_sha1 = Digest::SHA1.hexdigest(brief_id).to_i(16).to_s(36)
@@ -90,28 +110,28 @@ class Script
             doc.gsub!('#BEITRAG_SESB_3.', "#{BEITRAG_SESB_3.to_s} €")
             doc.gsub!('#SUS_KLASSENSTUFE.', klassenstufe_next.to_s)
             doc.gsub!('#SUS_ZUG_DATIV.', sesb ? 'SESB-Zug' : 'altsprachlichen Zug')
+            doc.gsub!('#VERWENDUNGSZWECK.', subject)
 
-            amount = 1.23
+            amount = 0
 
-            doc.gsub!('#BETRAG.', sprintf('%.2f €', amount).sub('.', ','))
-
-            sibling_index_next_year = @@user_info[email][:sibling_index_next_year]
+            sibling_index_next_year = @@user_info[email][:sibling_index_next_year] || 0
             satz = StringIO.open do |io|
                 if sibling_index_next_year == 0
-                    io.puts "Da uns keine weiteren, älteren Geschwisterkinder bekannt sind, beträgt der Beitrag für Ihr Kind im nächsten Schuljahr #{BEITRAG_AS_1} €."
+                    amount = sesb ? BEITRAG_SESB_1 : BEITRAG_AS_1
+                    io.puts "Da uns keine weiteren, älteren Geschwisterkinder bekannt sind, beträgt der Beitrag für Ihr Kind im nächsten Schuljahr #{sprintf('%.2f', amount).sub('.', ',')} €."
                 elsif sibling_index_next_year == 1
+                    amount = sesb ? BEITRAG_SESB_2 : BEITRAG_AS_2
                     older_siblings = join_with_sep(@@user_info[email][:older_siblings].reverse.map { |x| @@user_info[x][:display_first_name] }, ', ', ' und ')
-                    io.puts "Da uns ein weiteres, älteres Geschwisterkind bekannt ist (#{older_siblings}), beträgt der Beitrag für Ihr Kind im nächsten Schuljahr #{BEITRAG_AS_2} €."
+                    io.puts "Da uns ein weiteres, älteres Geschwisterkind bekannt ist (#{older_siblings}), beträgt der Beitrag für Ihr Kind im nächsten Schuljahr #{sprintf('%.2f', amount).sub('.', ',')} €."
                 elsif sibling_index_next_year >= 2
+                    amount = sesb ? BEITRAG_SESB_3 : BEITRAG_AS_3
                     older_siblings = join_with_sep(@@user_info[email][:older_siblings].reverse.map { |x| @@user_info[x][:display_first_name] }, ', ', ' und ')
-                    io.puts "Da uns #{sibling_index_next_year > 2 ? 'mindestens ' : ''}zwei weitere, ältere Geschwisterkinder bekannt sind (#{older_siblings}), beträgt der Beitrag für Ihr Kind im nächsten Schuljahr #{BEITRAG_AS_3} €."
+                    io.puts "Da uns #{sibling_index_next_year > 2 ? 'mindestens ' : ''}zwei weitere, ältere Geschwisterkinder bekannt sind (#{older_siblings}), beträgt der Beitrag für Ihr Kind im nächsten Schuljahr #{sprintf('%.2f', amount).sub('.', ',')} €."
                 end
                 io.string
             end
             doc.gsub!('#SUS_GESCHWISTER_UND_BEITRAG_SATZ.', satz)
-
-            #girocode EMPFAENGER.gsub(' ', '')
-
+            doc.gsub!('#BETRAG.', sprintf('%.2f €', amount).sub('.', ','))
 
             File.open(File.join(out_path_dir, formular_sha1, 'word', 'document.xml'), 'w') do |f|
                 f.write doc
@@ -130,7 +150,7 @@ class Script
 
             command = "cd \"#{File.join(out_path_dir, formular_sha1)}\"; zip -r \"#{out_path_docx}\" ."
             system(command)
-            # FileUtils::rm_rf(File.join(out_path_dir))
+            FileUtils::rm_rf(File.join(out_path_dir))
             command = "HOME=/internal/lowriter_home lowriter --headless --convert-to 'pdf:writer_pdf_Export:{\"ExportFormFields\":{\"type\":\"boolean\",\"value\":\"false\"}}' \"#{out_path_docx}\" --outdir \"#{File.dirname(out_path_docx)}\""
             STDERR.puts command
             system(command)

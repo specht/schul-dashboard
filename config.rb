f120217b25b58ce11cb6b7ea22361d45f9607873
Michael Specht <micha.specht@gmail.com>
2024-04-29 16:49:53 +0200

    yay

--- config.rb
+++ config.rb
@@ -241,9 +241,7 @@ if DEVELOPMENT
     docker_compose[:services][:invitation_bot][:ports] = ['127.0.0.1:8023:8080']
 else
     docker_compose[:services].each_pair do |k, x|
-        STDERR.puts "k = #{k}"
-        x[:restart] = (k == 'ruby2') ? :no : :always
-        STDERR.puts "x = #{x.to_json}"
+        x[:restart] = (k.to_s == 'ruby2') ? :no : :always
     end
 end
 

ea500301515fa11b7964a104b0f2c41176227e17
Michael Specht <micha.specht@gmail.com>
2024-07-06 14:05:55 +0200

    yay

--- src/ruby/projekttage-assign-single-sus.rb
+++ src/ruby/projekttage-assign-single-sus.rb
@@ -21,7 +21,7 @@ class Script
     def run()
         STDERR.print "Searching for user #{@user}: "
         rows = @@user_info.keys.select do |email|
-            @@user_info[email][:email].include?(@user)
+            email.include?(@user)
         end
         assert(rows.length == 1, "User not found")
         user = rows[0]

1a6488c143a80e89dc08491519b25dd63c7a01ee
Michael Specht <micha.specht@gmail.com>
2024-08-30 21:58:33 +0200

    yay

--- src/ruby/print-email-letters.rb
+++ src/ruby/print-email-letters.rb
@@ -89,7 +89,8 @@ class Script
                 entry.gsub!('#{LAST_NAME}', record[:last_name])
                 entry.gsub!('#{DISPLAY_NAME}', record[:display_name])
                 entry.gsub!('#{DISPLAY_LAST_NAME}', record[:display_last_name])
-                entry.gsub!('#{KLASSE}', record[:klasse].gsub('o', '\\textomega'))
+                klasse = record[:klasse]
+                entry.gsub!('#{KLASSE}', (KLASSEN_TR[klasse] || klasse).gsub('ω', '\\textomega'))
                 entry.gsub!('#{PASSWORD}', password)
                 entry.gsub!('#{PASSWORD_PARENTS}', password_parents)
                 entry.gsub!('#{INITIALS}', "#{(record[:display_first_name][0] || '').upcase}#{record[:last_name][0].upcase}")
